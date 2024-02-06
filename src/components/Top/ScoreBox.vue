2e8410e247a9393d549d2fbed5d7e3e3ae8900e2
Misiuu <michuu9816@gmail.com>
2024-02-06 21:18:50 +0100

    9atNine

--- src/components/Top/ScoreBox.vue
+++ src/components/Top/ScoreBox.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, computed } from "vue";
+import { defineProps, computed, ref, watch } from "vue";
 import { useMatchStore } from "@/stores/match";
 
 const matchStore = useMatchStore();
@@ -8,14 +8,33 @@ const props = defineProps(["team"]);
 
 const score = computed(() => {
 	let matchData = matchStore.getData();
-	return matchData ? matchData[`team_${props.team}`].score : "-";
+	return matchData ? (showNine.value ? "IX" : matchData[`team_${props.team}`].score) : "-";
 });
+
+const showNine = ref(false);
+
+const isNineAt9 = computed(() => {
+	let matchData = matchStore.getData();
+	return matchData ? matchData[`team_${props.team}`]?.score == 9 && matchData[`team_${props.team}`]?.name?.toUpperCase() == "9INE" : false
+})
+
+watch(isNineAt9, (val) => {
+	if (val) {
+		setTimeout(() => {
+			showNine.value = val;
+		}, 3800)
+	} else {
+		showNine.value = val;
+	}
+})
 
 </script>
 
 <template>
 	<div class="score" :class="[team]">
-		<div class="text">{{ score }}</div>
+		<div class="text" :class="{
+			nine: isNineAt9
+		}">{{ score }}</div>
 	</div>
 </template>
 
@@ -52,6 +71,44 @@ const score = computed(() => {
 
 .score.t .text {
 	transform: skewX(10deg);
+}
+
+.score .text.nine {
+	animation: nineAt9 5s;
+	transition: transform 0s 3.8s, font-family 0s 5s;
+	font-family: Arial, Helvetica, sans-serif;
+	transform: rotate(270deg) skewY(-10deg);
+}
+
+.score.ct .text.nine {
+	transform: rotate(270deg) skewY(10deg);
+}
+
+@keyframes nineAt9 {
+
+	20% {
+		font-style: italic;
+	}
+
+	31%,
+	40%,
+	71%,
+	100% {
+		opacity: 0;
+	}
+
+	0%,
+	30%,
+	41%,
+	70% {
+		opacity: 1;
+		font-family: inherit;
+	}
+
+	80% {
+		font-family: Arial, Helvetica, sans-serif;
+	}
+
 }
 </style>
 
